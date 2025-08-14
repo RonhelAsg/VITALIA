0954c26c61a69e0f83327e194fa5566f0975d185
Ronhel Asg <ronhel746@gmail.com>
2025-08-14 13:25:07 +0100

    Création des pages de lancement

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:vitalia/screens/first_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => FirstPage()), // Redirection
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset('assets/images/logo_vitalia.png',
+          width: 200,
+          height: 200,
+        ),
+      ),
+    );
+  }
+}
